743d2f49ac8dca159b99c5a52ae7b2053b5efea2
Katricia Herring <kherring@nd.edu>
2017-03-24 15:00:50 -0700

    init test of makefile

---
+++ project4/config.c
@@ -0,0 +1,13 @@
+// config.cpp
+// Basic test driver for ConfigProcessor class
+// Author: Kat Herring
+// Date: 3/24/17
+
+#include "config.h"
+
+using namespace std;
+
+int main() {
+  cout << "Test" << endl;
+  return 0;
+}
