a8df6ab2105bf22b6773740c5b7edcfcd033d267
Katricia Herring <kherring@nd.edu>
2017-03-27 14:34:36 -0700

    config.h

--- project4/config.c
+++ project4/config.c
@@ -7,7 +7,18 @@
 
 using namespace std;
 
-int main() {
-  cout << "Test" << endl;
+int main(int argc, char *argv[]) {
+  int parseError;
+  if(argc!=2) {
+    cout << "Err: No config file provided!" << endl;
+    return 1;
+  }
+  string fName = argv[1];
+
+  ConfigProcessor test;
+  parseError = test.readFile(fName);
+  if(!parseError) {
+    test.display();
+  }
   return 0;
 }
--- project4/config.h
+++ project4/config.h
@@ -1,7 +1,134 @@
+// config.h
+// Class definition for ConfigProcessor
+// Processes input config file and displays to stdout
+
 #ifndef CONFIG_H
 #define CONFIG_H
 
 #include <iostream>
-#include <curl/curl.h>
+#include <string>
+#include <fstream>
+#include <stdexcept>
 
+using namespace std;
+
+class ConfigProcessor {
+  public:
+    ConfigProcessor();
+    int readFile(string f);
+    void display(); //display current settings
+  private:
+    int setArg(string arg, string val);
+    int stoiWrapper(string val);
+    void checkDefault();
+    const string validParam[5] =
+      { "PERIOD_FETCH",
+      "NUM_FETCH",
+      "NUM_PARSE",
+      "SEARCH_FILE",
+      "SITE_FILE" };
+    string configFile;
+    int PERIOD_FETCH;
+    int NUM_FETCH;
+    int NUM_PARSE;
+    string SEARCH_FILE;
+    string SITE_FILE;
+};
+ConfigProcessor::ConfigProcessor() {
+  PERIOD_FETCH = 180;
+  NUM_FETCH = 1;
+  NUM_PARSE = 1;
+  SEARCH_FILE = "Search.txt";
+  SITE_FILE = "Sites.txt";
+}
+void ConfigProcessor::checkDefault() {
+  string base = "Warning: Using default paramater for ";
+  if(PERIOD_FETCH==180) {
+    cout << base << "PERIOD_FETCH: " << PERIOD_FETCH << endl;
+  }
+  if(NUM_FETCH==1) {
+    cout << base << "NUM_FETCH: " << NUM_FETCH << endl;
+  }
+  if(NUM_PARSE==1) {
+    cout << base << "NUM_PARSE: " << NUM_PARSE << endl;
+  }
+  if(SEARCH_FILE=="Search.txt") {
+    cout << base << "SEARCH_FILE: " << SEARCH_FILE << endl;
+  }
+  if(SITE_FILE=="Sites.txt") {
+    cout << base << "SITE_FILE: " << SITE_FILE << endl;
+  }
+}
+int ConfigProcessor::readFile(string f) {
+  char delim = '=';
+  string line, arg, val;
+  size_t pos;
+  ifstream inFile(f);
+
+  if(inFile) {
+    while(getline(inFile, line)) {
+      if((pos = line.find(delim)) != string::npos) {
+        arg = line.substr(0, pos);
+        val = line.substr(pos+1); //skip delim
+        int parseError = setArg(arg, val);
+        if(parseError) {
+          return 1;
+        }
+      } else {
+        cout << "Warning: Unable to parse line '" << line << "'. Skipping..." << endl;
+      }
+    }
+  } else {
+    cout << "Err: File '" << configFile << "' not found!" << endl;
+    return 1;
+  }
+  checkDefault();
+  inFile.close();
+  return 0;
+}
+int ConfigProcessor::setArg(string arg, string val) {
+  if(arg=="PERIOD_FETCH") {
+    PERIOD_FETCH = stoi(val);
+  } else if(arg=="NUM_FETCH") {
+    if(stoiWrapper(val)!=-1) {
+      NUM_FETCH = stoiWrapper(val);
+    }
+  } else if(arg=="NUM_PARSE") {
+    NUM_PARSE = stoiWrapper(val);
+  } else if(arg=="SEARCH_FILE") {
+    if(!ifstream(val)) {
+      cout << "Err: Provided file name '" << val << "' does not exist! Exiting..." << endl;
+      return 1;
+    } else {
+      SEARCH_FILE = val;
+    }
+  } else if(arg=="SITE_FILE") {
+    if(!ifstream(val)) {
+      cout << "Err: Provided file name '" << val << "' does not exist! Exiting..." << endl;
+      return 1;
+    } else {
+    SITE_FILE = val;
+    }
+  } else {
+    cout << "Warning: Invalid arg '" << arg << "'. Skipping..." << endl;
+  }
+  return 0;
+}
+int ConfigProcessor::stoiWrapper(string val) {
+  int v;
+  try {
+    v = stoi(val);
+  } catch (const invalid_argument& ia) {
+    cerr << "Warning: Invalid argument '" << val << "'. Skipping..." << endl;
+    return -1;
+  }
+  return v;
+}
+void ConfigProcessor::display() {
+  cout << "PERIOD_FETCH=" << PERIOD_FETCH << endl;
+  cout << "NUM_FETCH=" << NUM_FETCH << endl;
+  cout << "NUM_PARSE=" << NUM_PARSE << endl;
+  cout << "SEARCH_FILE=" << SEARCH_FILE << endl;
+  cout << "SITE_FILE=" << SITE_FILE << endl;
+}
 #endif
